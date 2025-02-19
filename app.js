13f4506519fb5e660dd38d67766288b32addb79b
JoseVS1 <105169283+JoseVS1@users.noreply.github.com>
2025-02-18 20:43:35 -0600

    Configure CORS to allow requests from frontend origin

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const app = express();
 
 require("dotenv").config();
 
-app.use(cors());
+const corsOptions = {
+    origin: 'https://wheres-waldo-frontend-csxj.onrender.com',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  };
+  
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
